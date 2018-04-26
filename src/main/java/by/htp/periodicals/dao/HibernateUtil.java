8f6b64434de06e78404b1f7157b952539c86bd45
VladSVBY <vladossv@gmail.com>
2018-04-26 23:44:18 +0300

    HibernateUtil added

---
+++ src/main/java/by/htp/periodicals/dao/HibernateUtil.java
@@ -0,0 +1,30 @@
+package by.htp.periodicals.dao;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public final class HibernateUtil {
+	
+	private HibernateUtil() {
+	}
+	
+	private static final SessionFactory SESSION_FACTORY;
+
+	static {
+		try {
+			Configuration configuration = new Configuration();
+			configuration = configuration.configure();
+			StandardServiceRegistryBuilder serviceRegistry = new StandardServiceRegistryBuilder();
+			serviceRegistry = serviceRegistry.applySettings(configuration.getProperties());
+			SESSION_FACTORY = configuration.buildSessionFactory(serviceRegistry.build());
+		} catch (Exception e) {
+			// TODO: add log
+			throw new ExceptionInInitializerError(e);
+		}
+	}
+	
+	public static SessionFactory getSessionFactory() {
+		return SESSION_FACTORY;
+	}
+}
