b397e549cfbcddb19ce214c3ac31df25690323c4
VladSVBY <vladossv@gmail.com>
2018-04-26 22:25:39 +0300

    First commit
    User and Role entities added

---
+++ src/main/java/by/htp/periodicals/domain/Role.java
@@ -0,0 +1,30 @@
+package by.htp.periodicals.domain;
+
+public class Role {
+	
+	private int id;
+	private String role;
+	
+	public Role() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+
+}
---
+++ src/main/java/by/htp/periodicals/domain/User.java
@@ -0,0 +1,74 @@
+package by.htp.periodicals.domain;
+
+public class User {
+	
+	private int id;
+    private String login;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Role role;
+    
+	public User() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String name) {
+		this.firstName = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+    
+}
